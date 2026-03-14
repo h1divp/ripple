0a68ffd87bd0a5cda62226b2f8b72add14aeec2e
h1divp <71522316+h1divp@users.noreply.github.com>
2026-03-14 03:15:04 -0400

    refactor: use modified dsn for go connection, use old var only with bob

--- internal/config/config.go
+++ internal/config/config.go
@@ -11,7 +11,7 @@ import (
 
 type Config struct {
 	Port                 string   `env:"PORT" envDefault:"8080"`
-	DatabaseURL          string   `env:"PSQL_DSN"`
+	DatabaseURL          string   `env:"DATABASE_URL"`
 	AllowedOriginsString string   `env:"ALLOWED_ORIGINS"`
 	AllowedOrigins       []string ``
 }
