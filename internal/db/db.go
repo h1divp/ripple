dffbb321a671677f1211d9ab63fa37e81ae33d2f
h1divp <71522316+h1divp@users.noreply.github.com>
2026-03-14 02:10:31 -0400

    refactor: use db pool, add bob executor

--- internal/db/db.go
+++ internal/db/db.go
@@ -5,8 +5,12 @@ import (
 	"database/sql"
 	"embed"
 
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/jackc/pgx/v5/stdlib"
 	"github.com/pressly/goose/v3"
 	"github.com/stephenafamo/bob"
+
+	"github.com/h1divp/echo-chat-v2/internal/models"
 
 	_ "github.com/jackc/pgx/v5"
 )
@@ -14,18 +18,33 @@ import (
 var embedMigrations embed.FS
 
 type Store struct {
-	db bob.DB
+	db   bob.Executor
+	pool *pgxpool.Pool
+	repo *models.Store
 }
 
-func New(dsn string) (*Store, error) {
-	sqlDB, err := sql.Open("pgx", dsn)
+func New(connString string) (*Store, error) {
+	ctx := context.Background()
+
+	config, err := pgxpool.ParseConfig(connString)
 	if err != nil {
 		return nil, err
 	}
-	if err := sqlDB.Ping(); err != nil {
+
+	pool, err := pgxpool.NewWithConfig(ctx, config)
+	if err != nil {
 		return nil, err
 	}
 
+	sqlDB := stdlib.OpenDBFromPool(pool)
+	if err := sqlDB.Ping(); err != nil {
+		pool.Close()
+		return nil, err
+	}
+
+	executor := bob.NewDB(sqlDB)
+
+	// Run migrations
 	goose.SetBaseFS(embedMigrations)
 	if err := goose.SetDialect("postgres"); err != nil {
 		return nil, err
@@ -34,15 +53,32 @@ func New(dsn string) (*Store, error) {
 		return nil, err
 	}
 
-	return &Store{db: bob.NewDB(sqlDB)}, nil
+	return &Store{
+		db:   executor,
+		pool: pool,
+		repo: models.New(executor),
+	}, nil
+
 }
 
 func (s *Store) Close() error {
-	return s.db.DB.Close()
+	s.pool.Close()
+	return nil
 }
 
-func (s *Store) BeginTx(ctx context.Context, opts *sql.TxOptions) (*sql.Tx, error) {
-	return s.db.DB.BeginTx(ctx, opts)
+func (s *Store) BeginTx(ctx context.Context, opts *sql.TxOptions) (*Store, *sql.Tx, error) {
+	tx, err := s.BeginTx(ctx, opts)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	txExecutor := bob.NewTx(tx)
+
+	return &Store{
+		db: txExecutor,
+		pool: s.pool,
+		repo: models.New(txExecutor)
+	}
 }
 
 func (s *Store) executor(tx *sql.Tx) bob.Executor {
