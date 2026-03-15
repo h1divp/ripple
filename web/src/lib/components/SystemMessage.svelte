a06f084ff539701387e1e340d90372cdb11ad9ff
h1divp <71522316+h1divp@users.noreply.github.com>
2026-03-15 05:10:10 -0400

    feat: system messages

---
+++ web/src/lib/components/SystemMessage.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+let { message } = $props();
+</script>
+
+<div class="my-4 flex items-center gap-4 px-4">
+  <div class="h-[1px] flex-1 bg-gray-500"></div>
+
+  <div class="flex flex-col items-center text-center">
+    <span class="text-xs font-bold uppercase tracking-widest text-gray-500">
+      {message}
+    </span>
+  </div>
+
+  <div class="h-[1px] flex-1 bg-gray-500"></div>
+</div>
+
