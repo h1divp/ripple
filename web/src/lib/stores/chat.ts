13e5fa889de3ed19e48ff921eb0b6d31192e5875
h1divp <71522316+h1divp@users.noreply.github.com>
2026-03-14 23:33:35 -0400

    feat: add ws connection to store

---
+++ web/src/lib/stores/chat.ts
@@ -0,0 +1,28 @@
+import { writable } from 'svelte/store';
+import type { Message } from '$lib/types/types';
+import { PUBLIC_WS_URL } from '$env/static/public';
+
+export const messages = writable<Message[]>([]);
+export const isConnected = writable(false);
+
+let socket: WebSocket;
+
+export function connect(displayName: string) {
+  // cannot send displayName over as json body since this is not http, so we use query params
+  const connUrl = `${PUBLIC_WS_URL}?displayName=${displayName}`
+  socket = new WebSocket(connUrl) 
+
+  socket.onopen = () => isConnected.set(true)
+  socket.onclose = () => isConnected.set(false)
+
+  socket.onmessage = (event) => {
+    const data = JSON.parse(event.data);
+    messages.update((prev) => [...prev, data]);
+  };
+}
+
+export function sendMessage(text: string) {
+  if (socket && socket.readyState === WebSocket.OPEN) {
+    socket.send(JSON.stringify({ text }));
+  }
+}
---
+++ web/src/lib/types/types.ts
@@ -0,0 +1,6 @@
+export interface Message {
+    id: string;
+    displayName: string;
+    text: string;
+    timestamp: number;
+}
