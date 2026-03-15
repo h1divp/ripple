11c639e1bdf4aafdda6cf1bae2d3d031e8185f57
h1divp <71522316+h1divp@users.noreply.github.com>
2026-03-14 20:14:22 -0400

    feat(user): add interfaces and change naming scheme to avoid conflict. edit struct.

--- internal/user/user.go
+++ internal/user/user.go
@@ -1,6 +1,9 @@
 package user
 
-import "context"
+import (
+	"context"
+	"net/http"
+)
 
 type User struct {
 	ID          string
@@ -10,17 +13,25 @@ type User struct {
 }
 
 type Session struct {
-	ID          string
-	DisplayName string
-	IPAddress   string
-	TrustScore  int
-	IsAnon      bool
+	ID          string `redis:"session_id"`
+	DisplayName string `redis:"display_name"`
+	IPAddress   string `redis:"ip_address"`
+	TrustScore  int    `redis:"trust_score"`
 }
 
-type Repository interface {
-	GetProfile(ctx context.Context, userID string) (*User, error)
+type UserRepository interface {
+	// CreateProfile(ctx context.Context, userID string) (*User, error)
+	// GetProfile(ctx context.Context, userID string) (*User, error)
 
 	SaveSession(ctx context.Context, s *Session) error
 	GetSession(ctx context.Context, sessionID string) (*Session, error)
-	UpdateLocation(ctx context.Context, sessionID string, lat, lon float64) error
+	// UpdateLocation(ctx context.Context, sessionID string, lat, lon float64) error
+}
+
+type UserService interface {
+	JoinChat(ctx context.Context, displayName string, ip string) (*Session, error)
+}
+
+type UserHandler interface {
+	Connect(w http.ResponseWriter, r *http.Request)
 }
