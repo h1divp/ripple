14514b20fe890c972605d3363784c0ecd2dcc7a4
h1divp <71522316+h1divp@users.noreply.github.com>
2026-03-15 05:39:00 -0400

    refactor: simplified styling

--- web/src/lib/components/ChatHeader.svelte
+++ web/src/lib/components/ChatHeader.svelte
@@ -4,21 +4,14 @@
 
 </script>
 
-<div class="mb-4 flex flex-row flex-wrap items-center justify-center gap-2 sm:justify-start">
-  <div
-    class="flex w-fit flex-row gap-3 rounded-lg border-4 border-solid border-sky-800 bg-sky-100 p-1 text-sky-900"
-  >
+<div class="mb-4 flex flex-row flex-wrap gap-2 justify-start">
     {#if isConnected}
-      <div class="rounded-lgp-2 flex items-center justify-center">
+      <div class="rounded-lg flex items-center justify-center border-4 border-solid border-sky-800 bg-sky-100 p-1 text-sky-900">
         <IconUsers size={20} class="mx-1" />
         <span class="pr-2 text-xl font-bold">
           {nearbyCounter}
         </span>
       </div>
-    {:else}
-      <div class="rounded-lgp-2 flex items-center justify-center">
-        <span class="px-2 text-xl font-bold">Loading</span>
-      </div>
     {/if}
-  </div>
+      <div class="invisible"></div>
 </div>
