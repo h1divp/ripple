d52869373c336ca477a0001f907a83ea3bf0a76c
h1divp <71522316+h1divp@users.noreply.github.com>
2026-03-14 20:15:11 -0400

    feat(user): add JoinChat()

--- internal/user/service.go
+++ internal/user/service.go
@@ -1,2 +1,33 @@
-// JoinChat
-// ChangeName
+package user
+
+import (
+	"context"
+	"github.com/google/uuid"
+)
+
+// TODO: JoinChat, ChangeName
+
+type Service struct {
+	repo *Repository
+}
+
+func NewService(repo *Repository) *Service {
+	return &Service{repo: repo}
+}
+
+func (s *Service) JoinChat(ctx context.Context, displayName string, ip string) (*Session, error) {
+	sessionID := uuid.New().String()
+
+	sess := &Session{
+		ID:          sessionID,
+		DisplayName: displayName,
+		IPAddress:   ip,
+		TrustScore:  0,
+	}
+
+	if err := s.repo.SaveSession(ctx, sess); err != nil {
+		return nil, err
+	}
+
+	return sess, nil
+}
