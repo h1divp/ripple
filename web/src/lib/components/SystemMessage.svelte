89f814d6a7821349dd867aef5efdb77ac248c85e
h1divp <71522316+h1divp@users.noreply.github.com>
2026-03-15 16:01:25 -0400

    fix: removed padding

--- web/src/lib/components/SystemMessage.svelte
+++ web/src/lib/components/SystemMessage.svelte
@@ -2,11 +2,11 @@
 let { message } = $props();
 </script>
 
-<div class="my-4 flex items-center gap-4 px-4">
+<div class="my-4 flex items-center gap-4">
   <div class="h-[1px] flex-1 bg-gray-500"></div>
 
   <div class="flex flex-col items-center text-center">
-    <span class="text-xs font-bold uppercase tracking-widest text-gray-500">
+    <span class="text-xs font-bold uppercase tracking-widest text-sky-900/80">
       {message}
     </span>
   </div>
