dfff2a1c31875daf98310bc129ef737026cf9b1c
h1divp <71522316+h1divp@users.noreply.github.com>
2026-03-26 15:51:52 -0400

    refactor: refactor handlers for ChatMessage and LocationUpdate. Add GetLocationFromUserID to repo

--- internal/chat/message.go
+++ internal/chat/message.go
@@ -2,22 +2,25 @@ package chat
 
 import (
 	"context"
+	"errors"
 
 	"github.com/google/uuid"
 )
+
+var ErrCouldNotGetUserID = errors.New("Could not get userID from context.")
 
 type Message interface {
 	Handle(s *Service, ctx context.Context) error
 }
 
 type ChatMessage struct {
-	ID        uuid.UUID `json:"id",omitempty`
+	ID        uuid.UUID `json:"id,omitempty"`
 	Text      string    `json:"text,omitempty"`
 	Timestamp int64     `json:"timestamp,omitempty"`
 }
 
 type SystemMessage struct {
-	ID uuid.UUID `json:"id",omitempty`
+	ID uuid.UUID `json:"id,omitempty"`
 }
 
 // Range included for future ability to send chat messages to users
@@ -46,38 +49,55 @@ type IconUpdate struct {
 // but in situations where there is a high volume of join/leaves we can use a delta
 // to prevent needing to send many messages.
 type NearbyUpdate struct {
-	Delta int `json:"delta",omitempty`
+	Delta int `json:"delta,omitempty"`
 }
 
 func (m *ChatMessage) Handle(s *Service, ctx context.Context) error {
-	// get lat & lon from user_locations. the db is our source of truth :)
+	userID, ok := ctx.Value(userIdKey).(uuid.UUID)
+	if !ok {
+		s.logger.Warn().Msg("Attempted to handle message by a user with no stored userID")
+		return ErrCouldNotGetUserID
+	}
+
+	lat, lon, err := s.repo.GetLocationFromUserID(ctx, userID)
+	if err != nil {
+		s.logger.Err(err).Msg("Could not get location from userID")
+	}
+
 	// TODO: convert from messageSearchRadius to range stored in redis for user.
-	nearbyIDs, err := s.repo.FindNearbyUserIDs(ctx, /*Latitude*/, /*Longitude*/, s.messageSearchRadius)
+	nearbyIDs, err := s.repo.FindNearbyUserIDs(ctx, lat, lon, s.messageSearchRadius)
 	if err != nil {
 		s.logger.Err(err).Msg("Could not find nearby users")
 	}
 
-	recipients := append(nearbyIDs)
-
-	logEvent.Int("count", len(recipients)).Msg("Delivering message")
-	s.hub.DeliverToLocalClients(recipients, msg)
+	s.logger.Debug().Int("count", len(nearbyIDs)).Msg("Delivering message")
+	// TODO: update DeliverToLocalClients to accept new message type
+	s.hub.DeliverToLocalClients(nearbyIDs, m)
 	// TODO: Put onto redis pub/sub
+	return nil
 }
 
-func (m *LocationUpdate) Handle() {
+func (m *LocationUpdate) Handle(s *Service, ctx context.Context) error {
 	// Update location in redis
-	// get userID 
-	err := s.repo.UpdateUserLocation(ctx, /*userID*/, m.Latitude, m.Longitude)
+	userID, ok := ctx.Value(userIdKey).(uuid.UUID)
+	if !ok {
+		s.logger.Warn().Msg("Attempted to handle message by a user with no stored userID")
+		return ErrCouldNotGetUserID
+	}
+
+	err := s.repo.UpdateUserLocation(ctx, userID, m.Latitude, m.Longitude)
 	if err != nil {
 		s.logger.Err(err).Msg("Failed to update user location")
 	}
 	return nil
 }
 
-func (m *UsernameChange) Handle() {
+func (m *UsernameUpdate) Handle(s *Service, ctx context.Context) error {
 	// Send to nearby users
+	return nil
 }
 
-func (m *IconChange) Handle() {
+func (m *IconUpdate) Handle(s *Service, ctx context.Context) error {
 	// Send to nearby users
+	return nil
 }
--- internal/chat/repository.go
+++ internal/chat/repository.go
@@ -1,13 +1,18 @@
 package chat
 
+// TODO: convert userIDs to uuids as passed values to ensure type integrity. Ensure that uuids stored as strings are de-serialized back to uuids correctly.
 import (
 	"context"
+	"errors"
 
+	"github.com/google/uuid"
 	"github.com/redis/go-redis/v9"
 	"github.com/rs/zerolog"
 )
 
 const userLocationsKey = "user_locations"
+
+var ErrNoLocationFound = errors.New("No location found for user.")
 
 type Repository struct {
 	logger zerolog.Logger
@@ -21,6 +26,20 @@ func NewRepository(logger zerolog.Logger, rdb *redis.Client) *Repository {
 	}
 }
 
+func (r *Repository) GetLocationFromUserID(ctx context.Context, userID uuid.UUID) (float64, float64, error) {
+	pos, err := r.rdb.GeoPos(ctx, userLocationsKey, userID.String()).Result()
+	if err != nil {
+		r.logger.Err(err).Msg("Could not get location from userID")
+		return 0, 0, err
+	}
+
+	if len(pos) == 0 || pos[0] == nil {
+		return 0, 0, ErrNoLocationFound
+	}
+
+	return pos[0].Latitude, pos[0].Longitude, nil
+}
+
 func (r *Repository) FindNearbyUserIDs(ctx context.Context, lat, lon, radius float64) ([]string, error) {
 	return r.rdb.GeoSearch(ctx, userLocationsKey, &redis.GeoSearchQuery{
 		Latitude:   lat,
@@ -30,16 +49,17 @@ func (r *Repository) FindNearbyUserIDs(ctx context.Context, lat, lon, radius flo
 	}).Result()
 }
 
-func (r *Repository) UpdateUserLocation(ctx context.Context, userID string, lat, lon float64) error {
+func (r *Repository) UpdateUserLocation(ctx context.Context, userID uuid.UUID, lat, lon float64) error {
 	err := r.rdb.GeoAdd(ctx, userLocationsKey, &redis.GeoLocation{
-		Name:      userID,
+		// TODO: fix
+		Name:      userID.String(),
 		Latitude:  lat,
 		Longitude: lon,
 	}).Err()
 
 	if err != nil {
 		r.logger.Err(err).
-			Str("userID", userID).
+			Str("userID", userID.String()).
 			Msg("Failed to update user location in Redis")
 		return err
 	}
