a43ca8e77b01be92137f3ccbef2cb0d66bfe7992
h1divp <71522316+h1divp@users.noreply.github.com>
2026-03-31 08:58:16 -0400

    chore: update comments

--- internal/chat/message.go
+++ internal/chat/message.go
@@ -63,7 +63,6 @@ func (s *Service) HandleChatMessage(ctx context.Context, m *types.ChatMessageInb
 }
 
 func (s *Service) HandleLocationUpdate(ctx context.Context, m *types.LocationUpdate, userID uuid.UUID) error {
-	// TODO: how should we get the userID?
 	hasPreviousLocation, err := s.repo.HasUserLocation(ctx, userID)
 	if err != nil {
 		s.logger.Err(err).Msg("Could not determine if a user has a location stored in Redis")
@@ -74,8 +73,10 @@ func (s *Service) HandleLocationUpdate(ctx context.Context, m *types.LocationUpd
 		s.logger.Err(err).Msg("Failed to update user location")
 	}
 
+	// s.logger.Debug().Float64("lat", m.Latitude).Float64("lon", m.Longitude).Msg("")
+
 	if !hasPreviousLocation {
-		// This is the first location ping
+		// First location ping
 		s.BroadcastJoinNearbyUpdates(ctx, userID)
 	}
 	return nil
